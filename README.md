a56cc11f75626d3213fb5084e40c925f520942b0
okanon <flat.pfw37gins.tab@gmail.com>
2017-06-22 18:08:24 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # gmail_oauth2
 Transmission of Gmail corresponding to OAuth 2.0 authentication using C # and .Net framework
+
+## Installation
+
+Read `gmail_oauth2.sln` in Visual Studio and use it. If the Nuget package is wrong, try cleaning the solution.
+
+## Features / How to use
+- Mail
+
+  Enter the source Gmail address and the destination address.
+  ```
+  msg.From = new MailAddress("from@gmail.com");
+  msg.To.Add("to@gmail.com");
+  ```
+  If there are multiple destinations, connect by "," and write.
+  ```
+  msg.To.Add("to1@gmail.com,to2@docomo.ne.jp,to3@yahoo.co.jp");
+  ```
+- Attachment
+
+  When sending an attached file, please write the Path and ContentType of the attached file.
+
+- SMTP Authentication
+
+  ![](https://raw.githubusercontent.com/okanon/gmail_oauth2/master/img/app_passwd.png)
+
+  SMTP authentication is possible using Gmail account and password, but access by Google security is blocked. To correct this problem, please sign in to Google with the sender's Gmail address and allow use of old applications or create a password from __[Sign-in & security](https://myaccount.google.com/security) > Signing in to Google > Password & sign-in method > App passwords__ use it
+
+  ![](https://raw.githubusercontent.com/okanon/gmail_oauth2/master/img/generate_app_passwd.png)
+
+  Once you close the form you will not be able to check your password, so it's better to save your password.In the next update, we plan to be able to automatically obtain App Password and send mail.
+
+## License
+
+This library is under [the MIT License (MIT)](LICENSE).
---
+++ gmail_oauth2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Google.Apis;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
+
+namespace gmail_oauth2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage msg = new MailMessage();
+            //System.Net.Mail.MailMessage
+
+            msg.From = new MailAddress("from@gmail.com");
+            msg.To.Add("to@gmail.com");
+            /*
+             * send Mail to multiple people
+             *  msg.To.Add("to1@gmail.com,to2@docomo.ne.jp,to3@yahoo.co.jp");
+             */
+
+            //Subject
+            msg.Subject = "test subject";
+            //text
+            msg.Body = "test text";
+
+            //Attachment
+            Attachment attachment = new Attachment("your attachment path");
+
+            //attachment.ContentType = new ContentType("image/png");
+            attachment.ContentType = new ContentType("content type");
+
+            msg.Attachments.Add(attachment);
+
+
+            //SMTP authentication
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+
+            client.UseDefaultCredentials = true;
+            client.Credentials = new NetworkCredential("from@gmail.com", "password or oauth2.0 App password");
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+        }
+
+        static string OAuth2()
+        {
+            /*
+             *Implementation soon 
+             */
+            return "";
+        }
+    }
+}
